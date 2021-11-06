d945df1b2ed3a5392f1b3d12f3e584a8c9c91744
Joelayo <joetech.ooj@gmail.com>
2021-11-06 17:43:24 +0100

    main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import './Home.css'
 import {Link} from 'react-router-dom';
-import AppsIcon from '@mui/icons-material/Apps';
 import { Avatar } from '@mui/material';
+import AppsRoundedIcon from '@mui/icons-material/AppsRounded';
 
 function Home() {
     return (
         <div className='home'>
             <div className='home__header'>
                 <div className='home__headerLeft'>
-                    <Link to='/about'>About</Link>
-                    <Link to='/store'>Store</Link>
                 </div>
                 <div className='home__headerRight'>
                     <Link to='/gmail'>Gmail</Link>
                     <Link to='/gmail'>Images</Link>
-                    <AppsIcon />
+                    <AppsRoundedIcon />
                     <Avatar />
                 </div>
             </div>
             <div className='home__body'>
-
+                <img src='https://internship.sidehustle.ng/img/logo-dark.64d45129.png' alt='logo' />
             </div>
         </div>
     );
