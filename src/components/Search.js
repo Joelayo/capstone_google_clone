0769fa61f7cbbac5c2e5ca786cb247b3255eae88
Ayda <se.ayda.sultan@gmail.com>
2021-11-07 10:31:51 +0300

    state functionality on search bar added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,46 @@
-import React from 'react'
-import '../styles/Search.css';
-import SearchIcon from '@mui/icons-material/Search';
-import MicIcon from '../components/MicIcon';
-import { Button } from '@mui/material';
+import { useState, React } from "react";
+import "../styles/Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "../components/MicIcon";
+import { Button } from "@mui/material";
+import sidehustle from "../sidehustle.png";
 
 function Search() {
-    return (
-        <form className='search'>
-            <div className='search__input'>
-                <SearchIcon className='search__inputIcon' />
-                <input />
-                <MicIcon />
-            </div>
+  const [text, setText] = useState(null);
+  const [image, setImage] = useState(sidehustle);
+  function handleChange(event) {
+    setText(event.target.value);
 
-            <div className='search__buttons'>
-                <Button variant='outlined' type='submit'>Google Search</Button>
-                <Button variant='outlined'>I'm Feeling Lucky</Button>
-            </div>
-        </form>
-    )
+    if (event.target.value.length != 0) {
+      setImage(null);
+    } else {
+      setImage(sidehustle);
+    }
+  }
+  return (
+    <>
+      <div className="home__body">
+        <div className="home__text">
+          <p> {text} </p>
+        </div>
+        <img src={image} className="App-logo" />
+      </div>
+      <form className="search">
+        <div className="search__input">
+          <SearchIcon className="search__inputIcon" />
+          <input onChange={handleChange} />
+          <MicIcon />
+        </div>
+
+        <div className="search__buttons">
+          <Button variant="outlined" type="submit">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeling Lucky</Button>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default Search
+export default Search;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,31 @@
-import React from 'react'
-import '../styles/Home.css'
-import Search from '../components/Search';
-import {Link} from 'react-router-dom';
-import { Avatar } from '@mui/material';
+import React from "react";
+import "../styles/Home.css";
+import Search from "../components/Search";
+import { Link } from "react-router-dom";
+import { Avatar } from "@mui/material";
 import Footer from "./Footer";
 import sidehustle from "../sidehustle.png";
-import AppsRoundedIcon from '@mui/icons-material/AppsRounded';
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
 
 function Home() {
-    return (
-        <div className='home'>
-            <div className='home__header'>
-                <div className='home__headerLeft'>
-                </div>
-                <div className='home__headerRight'>
-                    <Link to='/'>Gmail</Link>
-                    <Link to='/'>Images</Link>
-                    <AppsRoundedIcon />
-                    <Avatar />
-                </div>
-            </div>
-            <div className='home__body'>
-                <img 
-                    src={sidehustle} 
-                    className="App-logo" 
-                    alt="logo" 
-                />
-                <div className='home__inputContainer'>
-                    <Search />
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft"></div>
+        <div className="home__headerRight">
+          <Link to="/">Gmail</Link>
+          <Link to="/">Images</Link>
+          <AppsRoundedIcon />
+          <Avatar />
         </div>
+      </div>
+      <div className="home__body">
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
 export default Home;
